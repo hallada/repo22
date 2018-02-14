fee8d702b8f2d6e544b9e75400e9905392e5276f
Formation <Formation@192.168.2.33>
2018-02-14 16:51:03 +0100

    Commit1

--- src/test/java/jpsTest/Test.java
+++ src/test/java/jpsTest/Test.java
@@ -2,33 +2,48 @@ package jpsTest;
 
 import static org.junit.Assert.assertEquals;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Test {
-
+	WebDriver driver;
+	DesiredCapabilities dc;
+	//RemoteWebDriver driver;
+	@Before
+	public void setUpt() {
+		
+	}
+	
 	@org.junit.Test
-	public void test() throws InterruptedException {
-		WebDriver driver;
+	public void test() throws InterruptedException, MalformedURLException {
+		
 		String navo = System.getProperty("browser");
 		if(navo==null) {
-			driver = new ChromeDriver();
-			System.out.println("Default Chrome");
-		}
-		else if (navo.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver",
-					"C:\\Users\\Formation\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			System.out.println("Firefox");
-		} else {
-			driver = new ChromeDriver();
+			dc = DesiredCapabilities.chrome();
+			//driver = new ChromeDriver();
 			System.out.println("Chrome");
 		}
-
-
+		else if (navo.equals("firefox")) {
+			//System.setProperty("webdriver.gecko.driver","C:\\Users\\Formation\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+			//driver = new FirefoxDriver();
+			dc = DesiredCapabilities.firefox();
+			System.out.println("Firefox");
+		} else {
+			//driver = new ChromeDriver();
+			dc = DesiredCapabilities.chrome();
+			System.out.println("Default Chrome");
+		}
+		 
+		driver = new RemoteWebDriver(new URL("http://localhost:444/wd/hub"), dc);
 		driver.get("http://localhost:8282/jpetstore/");
 
 		// Premiere page
@@ -39,8 +54,7 @@ public class Test {
 		WebElement linkFish = driver.findElement(By.xpath("//*[@id=\"QuickLinks\"]/a[1]"));
 		linkFish.click();
 		// Vérification du tableau
-		assertEquals("Angelfish",
-				driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[2]")).getText());
+		assertEquals("Angelfish", driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[2]")).getText());
 		// Fermeture Fenetre + session
 		driver.quit();
 	}
